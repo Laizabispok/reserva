fdad02d4fddf89ad91c5bba41fc50acb5ddf8e30
laizabispok <laizabispok@gmail.com>
2024-05-13 08:43:34 -0300

    Criação dos models

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,16 @@
+/*
+index: listagem de sessoes
+store(add): ceriar uma sessao
+show (list): quando queremos listar uma UNICA sessao
+update: quando queremos alterar alguma sessao
+destroy (delete): quando queremos deletar uma sessao
+*/
+
+import User from '../models/User'
+import * as Yuo from 'yup'
+
+class SessionController {
+    async store(req,res) {
+        
+    }
+}
