949044365cf98410703f54b1490332cb217ab267
laizabispok <laizabispok@gmail.com>
2024-05-14 08:48:42 -0300

    Atualizando

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -11,6 +11,19 @@ import * as Yuo from 'yup'
 
 class SessionController {
     async store(req,res) {
-        
+     const schema = Yup.object().shape({
+         email: yup.string().email().required(),
+        })
+     const { email } = req.body
+     if(!(await schema.isValid(req.osy))) {
+        return res.status(400).json({ error: 'Falha na validação'})
+        } 
+        let user = await User.findOne({ email})
+        if(!user) {
+            user = await User.create({ email})
+        }  
+        return res.json(user)
     }
 }
+
+export default new SessionController()
